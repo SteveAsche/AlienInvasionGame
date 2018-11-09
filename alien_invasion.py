279f0b5629a5a222ea6b208d92f1101a8c67e8d9
SteveAsche <steve.asche@gmail.com>
2018-11-08 17:44:09 -0800

    Add files via upload
    First iteration of alien game file

---
+++ alien_invasion.py
@@ -0,0 +1,22 @@
+""" First attempt at an graphical game"""
+
+import sys
+import pygame
+from settings import Settings 
+
+def run_game():
+	# Initialize game and create screen object
+	pygame.init()
+	ai_settings = Settings()
+	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+	pygame.display.set_caption("Alien Invasion")
+	bg_color = ai_settings.bg_color 
+	while True:
+		#watch for keyboard input
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
+		screen.fill(bg_color)
+		pygame.display.flip()
+
+run_game()
---
+++ settings.py
@@ -0,0 +1,10 @@
+"""This is the settings file for the Alien Invastion Game """
+
+class Settings():
+	"""A class to store all the settings for Alien Invasion"""
+	def __init__(self):
+		"""Initialize Game Settings"""
+		#Screen Settings
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (230, 230, 230)
